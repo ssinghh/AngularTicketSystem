6f1c308ec4418a2b8f7e4cc4f092609f389fa5f9
git-user99 <shubhanjali.profile@gmail.com>
2020-04-20 18:17:15 -0400

    Initial code base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import {
+    MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TicketService} from './service/ticket.service';
+import { GetdetailComponent } from './getdetail/getdetail.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    GetdetailComponent,
+    HomeComponent
+  ], 
+  providers: [TicketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/getdetail/getdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-getdetail',
+  templateUrl: './getdetail.component.html',
+  styleUrls: ['./getdetail.component.css']
+})
+export class GetdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
